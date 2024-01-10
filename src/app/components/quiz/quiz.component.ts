71b78770340a0f45116f76f418d9a22c383860a8
anaBrandao <anacarolini633@gmail.com>
2024-01-09 23:23:37 -0300

    propriedades logicas

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -6,6 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quiz.component.css']
 })
 export class QuizComponent implements OnInit {
+  
+  title: string ="";
+
+  questions:any 
+  questionSelected: any
+
+  answers:string[] = [];
+  asnwerSelected: string = "";
+
+  questionIndex:number = 0;
+  questionMaxIndex:number = 0;
+
+  finished:boolean = false;
 
   constructor() { }
 
