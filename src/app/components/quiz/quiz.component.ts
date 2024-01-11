cb1d5e232f5922bf8fef57dc12dc018307cc85cf
anaBrandao <anacarolini633@gmail.com>
2024-01-11 13:03:05 -0300

    options

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import quiz_questions from '../../../assets/data/quiz_questions.json';
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
@@ -23,6 +24,32 @@ export class QuizComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    if(quiz_questions){
+      this.finished = false;
+      this.title = quiz_questions.title;
+
+      this.questions = quiz_questions.questions;
+      this.questionSelected = this.questions[this.questionIndex];
+
+      this.questionIndex = 0;
+      this.questionMaxIndex = this.questions.length;
+    }
+  }
+
+  playerChoose(value:string){
+    this.answers.push(value);
+
+    //console.log(value);
+  }
+
+  async nextStep(){
+    this.questionIndex+=1;
+
+    if(this.questionMaxIndex > this.questionIndex){
+      this.questionSelected = this.questions[this.questionIndex];
+    }else{
+      this.finished = true;
+    }
   }
 
 }
